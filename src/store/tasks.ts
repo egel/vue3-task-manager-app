bcfb8727c66b36d7ae6a252611947a8b22725348
Maciej Sypien <maciejsypien@gmail.com>
2024-08-15 13:05:30 +0200

    feat: add task persistence with localStorage

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -4,7 +4,7 @@ import { Task, TaskStatus, TaskUpsertForm } from "../services/api";
 /**
  * Define dummy initial list
  */
-function initStore(): Array<Task> {
+function initTaskStore(): Array<Task> {
   return [
     {
       id: "adfe3a1e79aa6", // simple random string for demo purpose, best use something like uuid
@@ -41,30 +41,53 @@ function generateRandomId(): string {
   return Math.random().toString(16).toString();
 }
 
-export const useTasksStore = defineStore("tasks", {
+function recoverTasks(): Array<Task> {
+  const tasks = localStorage.getItem(TASKS_STORE);
+  return tasks ? JSON.parse(tasks) : initTaskStore();
+}
+
+function saveTasksToLocalstorage(tasks: Array<Task>): void {
+  localStorage.setItem(TASKS_STORE, JSON.stringify(tasks));
+}
+
+export const TASKS_STORE = "tasks_store";
+
+export const useTasksStore = defineStore(TASKS_STORE, {
   state: () => ({
-    tasks: initStore(),
+    tasks: recoverTasks(),
   }),
   actions: {
-    addTask(taskUpsertForm: TaskUpsertForm) {
+    addTask(taskUpsertForm: TaskUpsertForm): void {
       const task: Task = {
         ...taskUpsertForm,
         id: generateRandomId(),
       };
       this.tasks.push(task);
+
+      // persist updated tasks
+      saveTasksToLocalstorage(this.tasks);
     },
     updateTask(task: Task) {
       const idx = this.tasks.findIndex((t: Task) => t.id == task.id);
       this.tasks[idx] = Object.assign({}, task);
+
+      // persist changed tasks
+      saveTasksToLocalstorage(this.tasks);
     },
     deleteTask(taskId: string) {
       this.tasks = this.tasks.filter((object: Task) => {
         return object.id !== taskId;
       });
+
+      // persist changed tasks
+      saveTasksToLocalstorage(this.tasks);
     },
     completeTask(taskId: string): void {
       const idx = this.tasks.findIndex((task: Task) => task.id == taskId);
       this.tasks[idx].status = TaskStatus.Completed;
+
+      // persist changed tasks
+      saveTasksToLocalstorage(this.tasks);
     },
   },
 });
